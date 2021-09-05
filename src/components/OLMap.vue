aca947061015048bfed432d484e96e818aedaff6
haji <zhuanghaji95@gmail.com>
2021-09-05 23:45:03 +0800

    首次提交

---
+++ src/components/OLMap.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="map">
+    <div class="location" id="location"></div>
+  </div>
+</template>
+
+<script>
+import "ol/ol.css";
+import { Map, View, Feature } from "ol";
+import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
+import { OSM, Vector as VectorSource } from "ol/source";
+import { Polygon, MultiPolygon, Point, Geometry } from "ol/geom";
+import GeoJSON from "ol/format/GeoJSON";
+import chinaMap from "../assets/中华人民共和国.json";
+import MousePositionControl from "ol/control/MousePosition";
+import { createStringXY } from "ol/coordinate";
+import { transformExtent, transform } from "ol/proj";
+import { Style, Stroke, Fill, Icon } from "ol/style";
+import TextStyle from "ol/style/Text";
+import Layer from "ol/layer/Layer";
+
+export default {
+  data() {
+    return {
+      extent: [70, 0, 136, 55],
+      map: null,
+      areaLayer: null,
+      zoom: 10
+    };
+  },
+  methods: {
+    initMap() {
+      var mousePositionControl = new MousePositionControl({
+        coordinateFormat: createStringXY(4),
+        projection: "ESPG:4326",
+        className: "custom-mouse-position",
+        target: "location",
+        undefinedHTML: "&nbsp;",
+      });
+
+      this.map = new Map({
+        target: "map",
+        layers: [
+          //   new TileLayer({
+          //     source: new OSM(),
+          //   }),
+        ],
+        view: new View({
+          center: transform([114.085947, 22.547], "EPSG:4326", "EPSG:3857"),
+          zoom: 10,
+          // minZoom: 20.5,
+          //   extends: transformExtent(this.extent, "EPSG:4326", "EPSG:3857"),
+        }),
+      });
+      this.map.addControl(mousePositionControl);
+      var _this = this;
+      this.map.on("moveend", function (e) {
+        _this.zoom = e.map.getView().getZoom();
+      });
+    },
+    /**
+     * 设置区域
+     */
+    addArea(geoFeatures = []) {
+      if (geoFeatures.length == 0) return false;
+      let areaFeatureStyle = new Style({
+        fill: new Fill({ color: "#4e98f444" }),
+        stroke: new Stroke({
+          width: 3,
+          color: [71, 137, 227, 1],
+        }),
+      });
+      let areaFeatures = [];
+      let areaFeature = null;
+      let cityNameFeatures = [];
+
+      // 设置图层
+      this.areaLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [],
+        }),
+      });
+      // 添加图层
+      this.map.addLayer(this.areaLayer);
+      geoFeatures.forEach((feature) => {
+        this.addCityName(feature);
+        if (feature.geometry.type == "MultiPolygon") {
+          areaFeature = new Feature({
+            geometry: new MultiPolygon(feature.geometry.coordinates).transform(
+              "EPSG:4326",
+              "EPSG:3857"
+            ),
+          });
+        } else if (feature.geometry.type == "Polygon") {
+          areaFeature = new Feature({
+            geometry: new Polygon(feature.geometry.coordinates).transform(
+              "EPSG:4326",
+              "EPSG:3857"
+            ),
+          });
+        }
+        areaFeature.setStyle(areaFeatureStyle);
+        areaFeatures.push(areaFeature);
+      });
+      this.areaLayer.getSource().addFeatures(areaFeatures);
+    },
+    addCityName(feature) {
+      let mark = new Feature({
+        type: "icon",
+        geometry: new Point(feature.properties.center).transform(
+          "EPSG:4326",
+          "EPSG:3857"
+        ),
+      });
+      this.map.addLayer(
+        new VectorLayer({
+          source: new VectorSource({
+            features: [mark],
+          }),
+          style: new Style({
+            text: new TextStyle({
+              text: feature.properties.name,
+              font: `${this.zoom * 1.2}px font-size`, // 设置字体大小
+              fill: new Fill({
+                // 设置字体颜色
+                color: "#1CAF9A",
+              }),
+            }),
+          }),
+        })
+      );
+    },
+  },
+  mounted() {
+    this.initMap(); //初始化地图方法
+    this.addArea(chinaMap.features); //添加区域图层方法
+  },
+};
+</script>
+
+<style>
+#map {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <OLMap />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import OLMap from '@/components/OLMap.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    OLMap
   }
 }
 </script>
